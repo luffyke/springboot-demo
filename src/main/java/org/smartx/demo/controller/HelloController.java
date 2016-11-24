4cc416f9229d200438df1c93ab29b7e8caa27c3a
kext <luffyke@gmail.com>
2016-11-24 16:04:05 +0800

    add spring loaded for hot deploy

--- src/main/java/org/smartx/demo/controller/HelloController.java
+++ src/main/java/org/smartx/demo/controller/HelloController.java
@@ -26,7 +26,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot!" + "\n" + "Hello Again";
     }
 
     @RequestMapping("/app")
