a9abfc94d1f6f5fe00d92d2b827f3c54dbe9e2a7
kext <luffyke@gmail.com>
2016-12-07 13:55:23 +0800

    logging

--- src/main/java/org/smartx/demo/controller/HelloController.java
+++ src/main/java/org/smartx/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package org.smartx.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smartx.demo.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Value("${app.name}")
     private String appName;
 
@@ -26,6 +30,8 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
+        logger.debug("debug log");
+        logger.info("info log");
         return "Hello Spring Boot!" + "\n" + "Hello Again";
     }
 
